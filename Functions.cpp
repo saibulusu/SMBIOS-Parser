fc851913669766c1624aa6735d4679f3b2e3074d
saibulusu <sai.bulusu@gmail.com>
2020-05-18 11:29:46 -0700

    Finished Type 4 Structure.

--- Functions.cpp
+++ Functions.cpp
@@ -734,7 +734,67 @@ void displayInformation(SMBIOS_struct_type_4* cur_struct, RawSMBIOSData* raw_dat
         return;
     }
 
+    std::cout << "L1 Cache Handle: " << (int)cur_struct->L1CacheHandle << std::endl;
+    std::cout << "L2 Cache Handle: " << (int)cur_struct->L2CacheHandle << std::endl;
+    std::cout << "L3 Cache Handle: " << (int)cur_struct->L3CacheHandle << std::endl;
 
+    if (raw_data->SMBIOSMajorVersion == 2 && raw_data->SMBIOSMinorVersion < 3) {
+        std::cout << "--------------------------------------------------------" << std::endl;
+        return;
+    }
+
+    if (cur_struct->SerialNumber == 0) {
+        std::cout << "There is no serial number information" << std::endl;
+    }
+    else {
+        std::cout << "Serial Number: " << strings[cur_struct->SerialNumber] << std::endl;
+    }
+
+    if (cur_struct->AssetTag == 0) {
+        std::cout << "There is no asset tag information" << std::endl;
+    }
+    else {
+        std::cout << "Asset Tag: " << strings[cur_struct->AssetTag] << std::endl;
+    }
+
+    if (cur_struct->PartNumber == 0) {
+        std::cout << "There is no part number information" << std::endl;
+    }
+    else {
+        std::cout << "Part Number: " << strings[cur_struct->PartNumber] << std::endl;
+    }
+
+    if (raw_data->SMBIOSMajorVersion == 2 && raw_data->SMBIOSMinorVersion < 5) {
+        std::cout << "--------------------------------------------------------" << std::endl;
+        return;
+    }
+
+    std::cout << "Core Count: " << (int)cur_struct->CoreCount << std::endl;
+    std::cout << "Core Enabled: " << (int)cur_struct->CoreEnabled << std::endl;
+    std::cout << "Thread Count: " << (int)cur_struct->ThreadCount << std::endl;
+
+    displayProcessorCharacteristics(cur_struct);
+    std::cout << "Processor Family 2: " << getProcessorFamily2(cur_struct) << std::endl;
+    if (cur_struct->CoreCount2 == 0) {
+        std::cout << "Core Count 2: Unkown" << std::endl;
+    }
+    else {
+        std::cout << "Core Count 2: " << (int)cur_struct->CoreCount2 << std::endl;
+    }
+
+    if (cur_struct->CoreEnabled2 == 0) {
+        std::cout << "Core Enabled 2: Unknown" << std::endl;
+    }
+    else {
+        std::cout << "Core Enabled 2: " << (int)cur_struct->CoreEnabled2 << std::endl;
+    }
+
+    if (cur_struct->ThreadCount2 == 0) {
+        std::cout << "Thread Count 2: Unknown" << std::endl;
+    }
+    else {
+        std::cout << "Thread Count 2: " << (int)cur_struct->ThreadCount2 << std::endl;
+    }
 
     std::cout << "--------------------------------------------------------" << std::endl;
 }
@@ -991,6 +1051,444 @@ std::string getProcessorUpgrade(SMBIOS_struct_type_4* cur_struct) {
         return "Socket BGA1234";
     case 48:
         return "Socket BGA1364";
+    default:
+        return "Other";
+    }
+}
+
+void displayProcessorCharacteristics(SMBIOS_struct_type_4* cur_struct) {
+    BYTE characteristics[100];
+    characteristics[0] = cur_struct->ProcessorCharacteristics;
+    std::cout << "Capabilities: ";
+    if (get_bit(characteristics, 2, 1) == 1) {
+        std::cout << "64-bit Capable ";
+    }
+    if (get_bit(characteristics, 3, 1) == 1) {
+        std::cout << "Multi-Core ";
+    }
+    if (get_bit(characteristics, 4, 1) == 1) {
+        std::cout << "Hardware Thread ";
+    }
+    if (get_bit(characteristics, 5, 1) == 1) {
+        std::cout << "Execute Protection ";
+    }
+    if (get_bit(characteristics, 6, 1) == 1) {
+        std::cout << "Enhanced Virtualization ";
+    }
+    if (get_bit(characteristics, 7, 1) == 1) {
+        std::cout << "Power/Performance Control ";
+    }
+    std::cout << std::endl;
+}
+
+std::string getProcessorFamily2(SMBIOS_struct_type_4* cur_struct) {
+    switch (cur_struct->ProcessorFamily2) {
+    case 1:
+        return "Other";
+    case 2:
+        return "Unknown";
+    case 3:
+        return "8086";
+    case 4:
+        return "80286";
+    case 5:
+        return "Intel386 Processor";
+    case 6:
+        return "Intel486 Processor";
+    case 7:
+        return "8087";
+    case 8:
+        return "80287";
+    case 9:
+        return "80387";
+    case 10:
+        return "80487";
+    case 11:
+        return "Intel Pentium Processor";
+    case 12:
+        return "Pentium Pro Processor";
+    case 13:
+        return "Pentium II Processor";
+    case 14:
+        return "Pentium Processor with MMX Technology";
+    case 15:
+        return "Intel Celeron Processor";
+    case 16:
+        return "Pentium II Xeon Processor";
+    case 17:
+        return "Pentium III Processor";
+    case 18:
+        return "M1 Family";
+    case 19:
+        return "M2 Family";
+    case 20:
+        return "Intel Celeron M Processor";
+    case 21:
+        return "Intel Pentium 4 HT Processor";
+    case 24:
+        return "AMD Curon Processor Family";
+    case 25:
+        return "K5 Family";
+    case 26:
+        return "K6 Family";
+    case 27:
+        return "K6-2";
+    case 28:
+        return "K6-3";
+    case 29:
+        return "AMD Athlon Processor Family";
+    case 30:
+        return "AMD29000 Family";
+    case 31:
+        return "K6-2+";
+    case 32:
+        return "Power PC Family";
+    case 33:
+        return "Power PC 601";
+    case 34:
+        return "Poewr PC 603";
+    case 35:
+        return "Power PC 603+";
+    case 36:
+        return "Power PC 604";
+    case 37:
+        return "Power PC 620";
+    case 38:
+        return "Power PC x704";
+    case 39:
+        return "Power PC 750";
+    case 40:
+        return "Intel Core Duo Processor";
+    case 41:
+        return "Intel Core Duo Mobile Processor";
+    case 42:
+        return "Intel Core Solo Mobile Processor";
+    case 43:
+        return "Intel Atom Processor";
+    case 44:
+        return "Intel Core M Processor";
+    case 48:
+        return "Alpha Family";
+    case 49:
+        return "Alpha 21064";
+    case 50:
+        return "Alpha 21066";
+    case 51:
+        return "Alpha 21164";
+    case 52:
+        return "Alpha 21164PC";
+    case 53:
+        return "Alpha 21164a";
+    case 54:
+        return "Alpha 21264";
+    case 55:
+        return "Alhpa 21364";
+    case 56:
+        return "AMD Turion II Ultra Dual-Core Mobile M Processor Family";
+    case 57:
+        return "AMD Turio II Dual-Core Mobile M Processor Family";
+    case 58:
+        return "AMD Athlon II Dual-Core M Processor Family";
+    case 59:
+        return "AMD Opteron 6100 Series Processor";
+    case 60:
+        return "AMD Opteron 4100 Series Processor";
+    case 61:
+        return "AMD Opteron 6200 Series Processor";
+    case 62:
+        return "AMD Opteron 4200 Series Processor";
+    case 63:
+        return "AMD FX Series Processor";
+    case 64:
+        return "MIPS Family";
+    case 65:
+        return "MIPS R4000";
+    case 66:
+        return "MIPS R4200";
+    case 67:
+        return "MIPS R4400";
+    case 68:
+        return "MIPS R4600";
+    case 69:
+        return "MIPS R10000";
+    case 70:
+        return "AMD C-Series Processor";
+    case 71:
+        return "AMD E-Series Processor";
+    case 72:
+        return "AMD A-Series Processor";
+    case 73:
+        return "AMD G-Series Processor";
+    case 74:
+        return "AMD Z-Series Processor";
+    case 75:
+        return "AMD R-Series Processor";
+    case 76:
+        return "AMD Opteron 4300 Series Processor";
+    case 77:
+        return "AMD Opteron 6300 Series Processor";
+    case 78:
+        return "AMD Opteron 3300 Series Processor";
+    case 79:
+        return "AMD FirePro Series Processor";
+    case 80:
+        return "SPARC Family";
+    case 81:
+        return "SuperSPARC";
+    case 82:
+        return "microSparc II";
+    case 83:
+        return "microSPARCK IIep";
+    case 84:
+        return "UltraSPARC";
+    case 85:
+        return "UltraSPARC II";
+    case 86:
+        return "UltraSPARC Iii";
+    case 87:
+        return "UltraSPARC III";
+    case 88:
+        return "UltraSPARC IIIi";
+    case 96:
+        return "68040 Family";
+    case 97:
+        return "68xxx";
+    case 98:
+        return "68000";
+    case 99:
+        return "68010";
+    case 100:
+        return "68020";
+    case 101:
+        return "68030";
+    case 102:
+        return "AMD Athlon(TM) X4 Quad-Core Processor Family";
+    case 103:
+        return "AMD Opteron(TM) X1000 Series Processor";
+    case 104:
+        return "AMD Opteron(TM) X2000 Series APU";
+    case 112:
+        return "Hobbit Family";
+    case 120:
+        return "Crusoe TM5000 Family";
+    case 121:
+        return "Crusoe TM3000 Family";
+    case 122:
+        return "Efficeon TM8000 Family";
+    case 128:
+        return "Weitek";
+    case 130:
+        return "Itanium Processor";
+    case 131:
+        return "AMD Athlon 64 Processor Fmaily";
+    case 132:
+        return "AMD Opteron Processor Family";
+    case 133:
+        return "AMD Sempron Processor Family";
+    case 134:
+        return "AMD Turion 64 Mobile Technology";
+    case 135:
+        return "Dual-Core AMD Opteron Processor Family";
+    case 136:
+        return "AMD Athlon 64 X2 Dual-Core Processor Family";
+    case 137:
+        return "AMD Turion 64 X2 Mobile Technology";
+    case 138:
+        return "Quad-Core AMD Opteron Processor Family";
+    case 139:
+        return "Thrid-Generation AMD Opteron Processor Family";
+    case 140:
+        return "AMD Phenom FX Quad-Core Processor Family";
+    case 141:
+        return "AMD Phenom X4 Quad-Core Processor Family";
+    case 142:
+        return "AMD Phenom X2 Dual-Core Processor Family";
+    case 143:
+        return "AMD Athlon X2 Dual-Core Processor Family";
+    case 144:
+        return "PA-RISC Family";
+    case 145:
+        return "PA-RISC 8500";
+    case 146:
+        return "PA-RISC 8000";
+    case 147:
+        return "PA-RISC 7300LC";
+    case 148:
+        return "PA-RISC 7200";
+    case 149:
+        return "PA-RISC 7100LC";
+    case 150:
+        return "PA-RISC 7100";
+    case 160:
+        return "V30 Family";
+    case 161:
+        return "Quad-Core Intel Xeon Processor 3200 Series";
+    case 162:
+        return "Dual-Core Intel Xeon Processor 3200 Series";
+    case 163:
+        return "Quad-Core Intel Xeon Processor 5300 Series";
+    case 164:
+        return "Dual-Core Intel Xeon Processor 5100 Series";
+    case 165:
+        return "Dual-Core Intel Xeon Processor 5000 Series";
+    case 166:
+        return "Dual-Core Intel Xeon Processor LV";
+    case 167:
+        return "Dual-Core Intel Xeon Processor ULV";
+    case 168:
+        return "Dual-Core Intel Xeon Processor 7100 Series";
+    case 169:
+        return "Quad-Core Intel Xeon Processor 5400 Series";
+    case 170:
+        return "Quad-Core Intel Xeon Processor";
+    case 171:
+        return "Dual-Core Intel Xeon Processor 5200 Series";
+    case 172:
+        return "Dual-Core Intel Xeon Processor 7200 Series";
+    case 173:
+        return "Quad-Core Intel Xeon Processor 7300 Series";
+    case 174:
+        return "Quad-Core Intel Xeon Processor 7400 Series";
+    case 175:
+        return "Multi-Core Intel Xeon Processor 7400 Series";
+    case 176:
+        return "Pentium III Xeon Processor";
+    case 177:
+        return "Pentium III Processor with Intel SpeedStep Technology";
+    case 178:
+        return "Pentium 4 Processor";
+    case 179:
+        return "Intel Xeon Processor";
+    case 180:
+        return "AS400 Family";
+    case 181:
+        return "Intel Xeon Processor MP";
+    case 182:
+        return "AMD Athlon XP Processor Family";
+    case 183:
+        return "AMD Athlon MP Processor Family";
+    case 184:
+        return "Intel Itanium 2 Processor";
+    case 185:
+        return "Intel Pentium M Processor";
+    case 186:
+        return "Intel Celeron D Processor";
+    case 187:
+        return "Intel Pentium D Processor";
+    case 188:
+        return "Intel Pentium Processor Extreme Edition";
+    case 189:
+        return "Intel Core Solo Processor";
+    case 191:
+        return "Intel Core 2 Duo Processor";
+    case 192:
+        return "Intel Core 2 Solo Processor";
+    case 193:
+        return "Intel Core 2 Extreme Processor";
+    case 194:
+        return "Intel Core 2 Quad Processor";
+    case 195:
+        return "Intel Core 2 Extreme Mobile Processor";
+    case 196:
+        return "Intel Core 2 Duo Mobile Processor";
+    case 197:
+        return "Intel Core 2 Solo Mobile Processor";
+    case 198:
+        return "Intel Core i7 Processor";
+    case 199:
+        return "Dual-Core Intel Celeron Processor";
+    case 200:
+        return "IBM390 Family";
+    case 201:
+        return "G4";
+    case 202:
+        return "G5";
+    case 203:
+        return "ESA/390 G6";
+    case 204:
+        return "z/Architecture Base";
+    case 205:
+        return "Intel Core i5 Processor";
+    case 206:
+        return "Intel Core i3 Processor";
+    case 210:
+        return "VIA C7-M Processor Family";
+    case 211:
+        return "VIA C7-D Processor Family";
+    case 212:
+        return "VIA C7 Processor Family";
+    case 213:
+        return "VIA Eden Processor Family";
+    case 214:
+        return "Multi-Core Intel Xeon Processor";
+    case 215:
+        return "Dual-Core Intel Xeon Processor 3xxx Series";
+    case 216:
+        return "Quad-Core Intel Xeon Processor 3xxx Series";
+    case 217:
+        return "VIA Nano Processor Family";
+    case 218:
+        return "Dual-Core Intel Xeon Processor 5xxx Series";
+    case 219:
+        return "Quad-Core Intel Xeon Processor 5xxx Series";
+    case 221:
+        return "Dual-Core Intel Xeon Processor 7xxx Series";
+    case 222:
+        return "Quad-Core Intel Xeon Processor 7xxx Series";
+    case 223:
+        return "Multi-Core Intel Xeon Processor 7xxx Series";
+    case 224:
+        return "Multi-Core Intel Xeon Processor 3400 Series";
+    case 228:
+        return "AMD Opteron 3000 Series Processor";
+    case 229:
+        return "AMD Sempron II Processor";
+    case 230:
+        return "Embedded AMD Opteron Quad-Core Processor Family";
+    case 231:
+        return "AMD Phenom Triple-Core Processor Family";
+    case 232:
+        return "AMD Turion Ultra Dual-Core Mobile Processor Family";
+    case 233:
+        return "AMD Turion Dual-Core Mobile Processor Family";
+    case 234:
+        return "AMD Athlon Dual-Core Processor Family";
+    case 235:
+        return "AMD Sempron SI Processor Family";
+    case 236:
+        return "AMD Phenom II Processor Family";
+    case 237:
+        return "AMD Athlon II Processor Family";
+    case 238:
+        return "Six-Core AMD Opteron Processor Family";
+    case 239:
+        return "AMD Sempron M Processor Family";
+    case 250:
+        return "i860";
+    case 251:
+        return "i960";
+    case 254:
+        return "Indicator to obtain the processor family from the Processor Family 2 Field";
+    case 260:
+        return "SH-3";
+    case 261:
+        return "SH-4";
+    case 280:
+        return "ARM";
+    case 281:
+        return "StrongARM";
+    case 300:
+        return "6x86";
+    case 301:
+        return "MediaGX";
+    case 302:
+        return "MII";
+    case 320:
+        return "WinChip";
+    case 350:
+        return "DSP";
+    case 500:
+        return "Video Processor";
     default:
         return "Other";
     }
